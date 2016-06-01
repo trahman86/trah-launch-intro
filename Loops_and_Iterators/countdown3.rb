af957742a530fbd183243c968df1eb31ca9f3bfd
Tiger Rahman <tiger.rahman86@gmail.com>
2016-05-31 22:30:37 -0400

    Updated Flow Control and began Loops and Iterators

---
+++ Loops_and_Iterators/countdown3.rb
@@ -0,0 +1,19 @@
+# Countdown with a for loop
+
+# x = gets.chomp.to_i
+
+# for i in 1..x do
+#   puts i 
+# end
+
+# puts "Done!"
+
+# countdown with an array
+
+x = [1,2,3,4,5]
+
+for i in x do 
+  puts i
+end
+
+puts "Done!"
