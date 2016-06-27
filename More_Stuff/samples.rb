64aee09f024d3562489a9b3c533dd2c6c652c282
Tiger Rahman <tiger.rahman86@gmail.com>
2016-06-27 01:51:57 -0400

    Cleanup work

--- More_Stuff/samples.rb
+++
@@ -1,7 +0,0 @@
-def test(b)
-  b.map! {|letter| "I like the letter: #{letter}"}
-end
-
-a = ['a','b','c']
-test(a)
-p a
--- Variables/Variables_Exercises.rb
+++
@@ -1,5 +0,0 @@
-#Exercise no. 5
-#In the second code snippet, the value of x is locally scoped to the method, and as such cannot be called outside of it (as never initialized in the global scope).
-
-#Exercise no. 6
-#This is another issue of scope; the variable 'shoes' is not defined where the program is trying to call it.
